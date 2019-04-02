9dc6663af3268b91cfaf31ba8e4cc78df1f831b8
MacOS <macos@MacOSs-Mac.local>
2019-04-02 10:09:39 -0700

    Initial Commit

---
+++ News Reader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News Reader
+//
+//  Created by MacOS on 4/2/19.
+//  Copyright © 2019 MacOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
